fe67307d7e624864762e2f220c9294049e21628b
GABRIEL PEREIRA FERREIRA <gabriel.pferreira7@senacsp.edu.br>
2022-11-23 19:45:17 -0300

    editar e apagar prontos

---
+++ crud/apagar.php
@@ -0,0 +1,17 @@
+<?php
+require_once '../bancoDeDados/conecta.php';
+
+$id = $_POST['id'] ?? 0;
+
+$id = preg_replace( '/\D/', '', $id);
+
+if ( $bd->exec("DELETE FROM alunos WHERE id = $id") ){
+
+    $apagou = true;
+
+}else{
+
+    $apagou = false;
+}
+
+include 'index.php';
